80e509c3af212d1828ca4c7428781bbd37011258
Mehmet Emin KARTAL <kartalbombe@icloud.com>
2017-12-26 19:07:29 +0300

    Initial Commit

---
+++ sahindeneme/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sahindeneme
+//
+//  Created by Muhammet Mehmet Emin Kartal on 12/26/17.
+//  Copyright © 2017 Mekatrotekno. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
