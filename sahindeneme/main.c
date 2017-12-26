c06d7b6816e5b18108fd1da793167b1554fa7cfa
Mehmet Emin KARTAL <kartalbombe@icloud.com>
2017-12-26 20:10:11 +0300

    asal sayı kontrolu

--- sahindeneme/main.c
+++ sahindeneme/main.c
@@ -1,56 +1,36 @@
 
-
 #include <stdio.h>
 
 
-int topla(int a, int b) {
-	return a + b;
+int asalmi(int sayi) {
+	
+	if (sayi % 2 == 0) {
+		return 0;
+	}
+	
+	for (int i = 2; i < sayi; i++) {
+		if(sayi % i == 0) {
+			return 0;
+		}
+	}
+	
+	return 1;
 }
 
-int cikart(int a, int b) {
-	return a - b;
-}
-
-int carp(int a, int b) {
-	return a * b;
-}
-
-float bol(int a, int b) {
-	return (float)a / (float)b;
-}
 
 int main() {
-	int a, b, islem;
 	
-	printf("1: Toplama\n2: Çıkartma\n3: Çarpma\n4: Bölme\n5: Çıkış\nİşlem Seçiniz: ");
-	scanf("%d", &islem);
+	//	30 ile 40 arasındaki asal sayıların toplamı
 	
-	printf("A sayısını giriniz: ");
-	scanf("%d", &a);
+	int toplam = 0;
 	
-	
-	printf("B sayısını giriniz: ");
-	scanf("%d", &b);
-	
-	
-	switch(islem) {
-		case 1:
-			printf("%d + %d = %d\n", a, b, topla(a, b));
-			break;
-		case 2:
-			printf("%d - %d = %d\n", a, b, cikart(a, b));
-			break;
-		case 3:
-			printf("%d * %d = %d\n", a, b, carp(a, b));
-			break;
-		case 4:
-			printf("%d / %d = %f\n", a, b, bol(a, b));
-			break;
-		case 5:
-			printf("Çıkış\n");
-			return 0;
-			break;
+	for(int i = 30; i <= 40; i++) {
+		if (asalmi(i)) {
+			toplam += i;
+		}
 	}
+	
+	printf("Toplam: %d\n", toplam);
 	
 	return 0;
 }
